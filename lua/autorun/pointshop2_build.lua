66d3f9f633ef392e80c5f85b92f1c22719958bbd
805camerons <805camerons@gmail.com>
2020-12-05 18:12:52 -0500

    Materials added

--- lua/autorun/pointshop2_build.lua
+++ lua/autorun/pointshop2_build.lua
@@ -1,1 +1,29 @@
-PS2_BUILD = "git"
+PS2_BUILD = "BTGaming"
+if SERVER then
+	resource.AddWorkshop("439856500")
+	
+	--[[
+		Role Passes
+	]]--
+	resource.AddFile("materials/pointshop2/detective.png")
+	resource.AddFile("materials/pointshop2/innocent.png")
+	resource.AddFile("materials/pointshop2/traitor.png")
+	
+	--[[
+		Weapon Passes
+	]]--
+	resource.AddFile("materials/pointshop2/ttt_deagle.png")
+	resource.AddFile("materials/pointshop2/ttt_discombobn.png")
+	resource.AddFile("materials/pointshop2/ttt_glock.png")
+	resource.AddFile("materials/pointshop2/ttt_huge.png")
+	resource.AddFile("materials/pointshop2/ttt_incen.png")
+	resource.AddFile("materials/pointshop2/ttt_m16.png")
+	resource.AddFile("materials/pointshop2/ttt_mac10.png")
+	resource.AddFile("materials/pointshop2/ttt_pistol.png")
+	resource.AddFile("materials/pointshop2/ttt_rifle.png")
+	resource.AddFile("materials/pointshop2/ttt_shotgun.png")
+	resource.AddFile("materials/pointshop2/ttt_smoke.png")
+
+	resource.AddFile("materials/pointshop2/karma_reset.png")
+	resource.AddFile("materials/pointshop2/pointconvert2.png")
+end
